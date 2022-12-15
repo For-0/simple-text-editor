c3604d2a23c2fd4029ab2a93d7f7f0015ffc3007
Siddhant <115331356+grimsteel@users.noreply.github.com>
2022-12-15 19:18:59 +0000

    Codemirror

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
+import { EditorView, basicSetup } from "codemirror"
+import { javascript } from "@codemirror/lang-javascript"
+const codemirrorContainer = document.getElementById("codemirror-container");
+
 document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.querySelector('.navbar-burger');
     navbar.addEventListener('click', () => {
         navbar.classList.toggle('is-active');
         document.querySelector("#main-navbar").classList.toggle('is-active');
     });
+});
+let editor = new EditorView({
+    extensions: [basicSetup, javascript()],
+    parent: codemirrorContainer
 });
