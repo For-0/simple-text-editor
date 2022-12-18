2c18af18d9aabfd37bda108cc9d46e847cdacffc
Siddhant <115331356+grimsteel@users.noreply.github.com>
2022-12-18 01:43:37 -0600

    Database and icon changes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
-import { EditorView, minimalSetup } from "codemirror";
-import { StateEffect } from "@codemirror/state";
-import { javascript } from "@codemirror/lang-javascript"
-import { html } from "@codemirror/lang-html";
-import { css } from "@codemirror/lang-css";
-import { lineNumbers, highlightActiveLineGutter, highlightActiveLine, keymap } from "@codemirror/view";
-import { foldGutter, indentOnInput, bracketMatching } from "@codemirror/language";
-import { closeBrackets, autocompletion, closeBracketsKeymap, completionKeymap } from '@codemirror/autocomplete';
+import { autocompletion, closeBrackets, closeBracketsKeymap, completionKeymap } from '@codemirror/autocomplete';
 import { indentWithTab } from "@codemirror/commands";
+import { css } from "@codemirror/lang-css";
+import { html } from "@codemirror/lang-html";
+import { javascript } from "@codemirror/lang-javascript";
 import { python } from "@codemirror/lang-python";
+import { bracketMatching, foldGutter, indentOnInput } from "@codemirror/language";
+import { StateEffect } from "@codemirror/state";
+import { highlightActiveLine, highlightActiveLineGutter, keymap, lineNumbers } from "@codemirror/view";
+import { EditorView, minimalSetup } from "codemirror";
+import FileMetadata from "./db";
 const codemirrorContainer = document.getElementById("codemirror-container");
+const filePanel = document.getElementById("panel-files-list");
 const languageSelect = document.getElementById("select-choose-language");
-const languageExtensions = [[], [html()], [css()], [javascript()], [python()]];
+const languageExtensions = {"text/plain": [], "text/html": [html()], "text/css": [css()], "application/javascript": [javascript()], "text/x-python": [python()]};
 const commonExtensions = [minimalSetup,
     lineNumbers(),
     highlightActiveLineGutter(),
@@ -26,19 +28,53 @@ const commonExtensions = [minimalSetup,
         indentWithTab
     ])
 ];
+
+let editor = new EditorView({
+    extensions: [...commonExtensions, ...languageExtensions[languageSelect.value]],
+    parent: codemirrorContainer
+});
+
+/**
+ * @param {FileMetadata} file The file to create a panel row for
+ */
+function createFileRow(file) {
+    const row = document.createElement("a");
+    row.classList.add("panel-block");
+    let fileIcon = row.appendChild(document.createElement("span"));
+    fileIcon.classList.add("panel-icon");
+    let icon = fileIcon.appendChild(new Image(24, 24));
+    icon.src = FileMetadata.FILE_ICONS[file.mimeType];
+    row.appendChild(document.createTextNode(file.name));
+    row.addEventListener("click", async () => {
+        const contents = await file.getContents();
+        languageSelect.value = file.mimeType;
+        editor.dispatch({
+            effects: StateEffect.reconfigure.of([...commonExtensions, ...languageExtensions[file.mimeType]])
+        });
+        editor.dispatch({
+            changes: {
+                from: 0,
+                to: editor.state.doc.length,
+                insert: contents
+            }
+        });
+    });
+    return row;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.querySelector('.navbar-burger');
     navbar.addEventListener('click', () => {
         navbar.classList.toggle('is-active');
         document.querySelector("#main-navbar").classList.toggle('is-active');
     });
-});
-let editor = new EditorView({
-    extensions: [...commonExtensions, ...languageExtensions[parseInt(languageSelect.value)]],
-    parent: codemirrorContainer
-});
-languageSelect.addEventListener("change", () => {
-    editor.dispatch({
-        effects: StateEffect.reconfigure.of([...commonExtensions, ...languageExtensions[parseInt(languageSelect.value)]])
+    FileMetadata.getAll().then(files => {
+        for (const file of files) filePanel.appendChild(createFileRow(file));
     });
-})
+    languageSelect.addEventListener("change", () => {
+        editor.dispatch({
+            effects: StateEffect.reconfigure.of([...commonExtensions, ...languageExtensions[languageSelect.value]])
+        });
+    });    
+});
+console.log(FileMetadata);
---
+++ src/db.js
@@ -0,0 +1,86 @@
+import { openDB } from "idb";
+
+export default class FileMetadata {
+    /**
+     * @param {Object} param0
+     * @param {string} param0.name Unique file name without an extension
+     * @param {("application/javascript"|"text/html"|"text/css"|"text/plain"|"text/x-python")} param0.mimeType Mimetype of the file
+     * @param {Date} param0.lastModified The last modified date of the file
+     * @param {Date} param0.created The date the file was created
+     */
+    constructor({name, mimeType, lastModified, created}) {
+        this.name = name;
+        this.mimeType = mimeType;
+        this.lastModified = lastModified;
+        this.created = created;
+    }
+    static FILE_ICONS = {
+        "application/javascript": new URL("icons/files/application-javascript.svg", import.meta.url),
+        "text/html": new URL("icons/files/text-html.svg", import.meta.url),
+        "text/css": new URL("icons/files/text-css.svg", import.meta.url),
+        "text/plain": new URL("icons/files/text-plain.svg", import.meta.url),
+        "text/x-python": new URL("icons/files/text-x-python.svg", import.meta.url)
+    }
+    static async getDB() {
+        return this.db || (this.db = await openDB("TextEditorDB", 1, {
+            upgrade(db, oldVersion, newVersion) {
+                if (oldVersion < 1 && newVersion === 1) {
+                    const fileMetaStore = db.createObjectStore("files_meta", {
+                        keyPath: "name",
+                        autoIncrement: false
+                    });
+                    fileMetaStore.createIndex("lastModified", "lastModified");
+                    fileMetaStore.createIndex("created", "created");
+                    fileMetaStore.createIndex("mimeType", "mimeType");
+                    db.createObjectStore("files", {
+                        keyPath: "name",
+                        autoIncrement: false
+                    });
+                }
+            },
+        }));
+    }
+    /**
+     * Get all of the stored files. Only retrievs metadata, not the contents
+     * @returns {Promise<FileMetadata[]>}
+     */
+    static async getAll() {
+        const db = await this.getDB();
+        const files = await db.getAll("files_meta");
+        return files.map(file => new FileMetadata(file));
+    }
+    /**
+     * Get the contents of this file from the files object store
+     * @returns {Promise<string>}
+     */
+    async getContents() {
+        const db = await FileMetadata.getDB();
+        const file = await db.get("files", this.name);
+        return file && await file.contentsBlob.text();
+    }
+    /**
+     * Saves the contents of the file into a blob in the files object store
+     * @param {string} contents The contents of the file
+     */
+    async setContents(contents) {
+        const db = await FileMetadata.getDB();
+        const contentsBlob = new Blob([contents], {type: this.mimeType});
+        await db.put("files", {name: this.name, contentsBlob});
+    }
+    /**
+     * Delete both the metadata and the contents of the file
+     */
+    async delete() {
+        const db = await FileMetadata.getDB();
+        const deleteTx = db.transaction(["files", "files_meta"], "readwrite");
+        await deleteTx.objectStore("files").delete(this.name);
+        await deleteTx.objectStore("files_meta").delete(this.name);
+    }
+    /**
+     * Save the metadata of the file into the files_meta object store
+     */
+    async save() {
+        const db = await FileMetadata.getDB();
+        await db.put("files_meta", {name: this.name, mimeType: this.mimeType, lastModified: this.lastModified, created: this.created});
+    }
+}
